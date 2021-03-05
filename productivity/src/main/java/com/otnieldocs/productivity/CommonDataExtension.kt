4315b5e030e5c35d7d827dfe47e4d1237f604ea6
otnieldocs <otnieldocs@gmail.com>
2021-03-05 16:48:45 +0700

    Create common data extension

---
+++ productivity/src/main/java/com/otnieldocs/productivity/CommonDataExtension.kt
@@ -0,0 +1,53 @@
+package com.otnieldocs.productivity
+
+import java.lang.NullPointerException
+
+// Int extension
+fun Int?.orZero(): Int = this ?: 0
+
+@Throws(NullPointerException::class)
+fun Int?.lessThan(value: Int): Boolean =
+    if (this == null) throw NullPointerException() else this.orZero() < value
+
+@Throws(NullPointerException::class)
+fun Int?.biggerThan(value: Int): Boolean =
+    if (this == null) throw NullPointerException() else this.orZero() > value
+
+// Long extension
+fun Long?.orZero(): Long = this ?: 0
+
+@Throws(NullPointerException::class)
+fun Long?.lessThan(value: Int): Boolean =
+    if (this == null) throw NullPointerException() else this.orZero() < value
+
+@Throws(NullPointerException::class)
+fun Long?.biggerThan(value: Int): Boolean =
+    if (this == null) throw NullPointerException() else this.orZero() > value
+
+// Float extension
+fun Float?.orZero(): Float = this ?: 0F
+
+@Throws(NullPointerException::class)
+fun Float?.lessThan(value: Int): Boolean =
+    if (this == null) throw NullPointerException() else this.orZero() < value
+
+@Throws(NullPointerException::class)
+fun Float?.biggerThan(value: Int) =
+    if (this == null) throw NullPointerException() else this.orZero() > value
+
+
+// Double extension
+fun Double?.orZero(): Double = this ?: 0.0
+
+@Throws(NullPointerException::class)
+fun Double?.lessThan(value: Int): Boolean =
+    if (this == null) throw NullPointerException() else this.orZero() < value
+
+@Throws(NullPointerException::class)
+fun Double?.biggerThan(value: Int) =
+    if (this == null) throw NullPointerException() else this.orZero() > value
+
+
+// Boolean extension
+fun Boolean?.orFalse() = this ?: false
+
---
+++ productivity/src/test/java/com/otnieldocs/productivity/CommonDataExtensionTest.kt
@@ -0,0 +1,186 @@
+package com.otnieldocs.productivity
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class CommonDataExtensionTest {
+    @Before
+    fun setUp() {}
+
+    // int
+
+    @Test
+    fun `Set the Int value to null, return 0`() {
+        val value: Int? = null
+        val actual: Int = value.orZero()
+
+        Assert.assertEquals(0, actual)
+    }
+
+    @Test
+    fun `Set the Int value to null, check lessThan 0, throw NPE`() {
+        val value: Int? = null
+
+        try {
+            value.lessThan(2)
+            Assert.assertTrue(false)
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(true)
+        }
+    }
+
+    @Test
+    fun `Set the Int value to -1, check is lessThan 0, return true`() {
+        val value: Int? = -1
+
+        try {
+            Assert.assertTrue(value.lessThan(0))
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(false)
+        }
+    }
+
+    @Test
+    fun `Set the Int value to 1, check is biggerThan 0, return true`() {
+        val value: Int? = 1
+
+        try {
+            Assert.assertTrue(value.biggerThan(0))
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(false)
+        }
+    }
+
+    // long
+
+    @Test
+    fun `Set the Long value to null, return 0`() {
+        val value: Long? = null
+        val actual = value.orZero()
+
+        Assert.assertEquals(0, actual)
+    }
+
+    @Test
+    fun `Set the Long value to null, check lessThan 0, throw NPE`() {
+        val value: Long? = null
+
+        try {
+            value.lessThan(2)
+            Assert.assertTrue(false)
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(true)
+        }
+    }
+
+    @Test
+    fun `Set the Long value to -1, check is lessThan 0, return true`() {
+        val value: Long? = -1
+
+        try {
+            Assert.assertTrue(value.lessThan(0))
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(false)
+        }
+    }
+
+    @Test
+    fun `Set the Long value to 1, check is biggerThan 0, return true`() {
+        val value: Long? = 1
+
+        try {
+            Assert.assertTrue(value.biggerThan(0))
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(false)
+        }
+    }
+
+    // float
+
+    @Test
+    fun `Set the Float value to null, return 0`() {
+        val value: Float? = null
+        val actual = value.orZero()
+
+        Assert.assertEquals(0F, actual)
+    }
+
+    @Test
+    fun `Set the Float value to null, check lessThan 0, throw NPE`() {
+        val value: Float? = null
+
+        try {
+            value.lessThan(2)
+            Assert.assertTrue(false)
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(true)
+        }
+    }
+
+    @Test
+    fun `Set the Float value to -1, check is lessThan 0, return true`() {
+        val value: Float? = -1F
+
+        try {
+            Assert.assertTrue(value.lessThan(0))
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(false)
+        }
+    }
+
+    @Test
+    fun `Set the Float value to 1, check is biggerThan 0, return true`() {
+        val value: Float? = 1F
+
+        try {
+            Assert.assertTrue(value.biggerThan(0))
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(false)
+        }
+    }
+
+    // double
+
+    @Test
+    fun `Set the Double value to null, return 0`() {
+        val value: Double? = null
+        val actual = value.orZero()
+
+        Assert.assertEquals(0.0, actual, 0.0)
+    }
+
+    @Test
+    fun `Set the Double value to null, check lessThan 0, throw NPE`() {
+        val value: Double? = null
+
+        try {
+            value.lessThan(2)
+            Assert.assertTrue(false)
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(true)
+        }
+    }
+
+    @Test
+    fun `Set the Double value to -1, check is lessThan 0, return true`() {
+        val value: Double? = -1.0
+
+        try {
+            Assert.assertTrue(value.lessThan(0))
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(false)
+        }
+    }
+
+    @Test
+    fun `Set the Double value to 1, check is biggerThan 0, return true`() {
+        val value: Double? = 1.0
+
+        try {
+            Assert.assertTrue(value.biggerThan(0))
+        } catch (e: NullPointerException) {
+            Assert.assertTrue(false)
+        }
+    }
+}
